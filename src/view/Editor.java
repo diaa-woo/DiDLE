12f2fa5fadf44180f1835a233c57558c8471b254
diadntjr <dntjr0501@gmail.com>
2023-06-15 13:44:45 +0900

    Update Editor
    Remove EditorPanel

--- src/view/Editor.java
+++ src/view/Editor.java
@@ -1,18 +1,36 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Editor extends JFrame {
-    private EditorPanel ep = new EditorPanel();
     private EditorBar eb = new EditorBar();
+    private Container myContainer = null;
 
     public Editor() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("SmallEditor");
         setSize(500, 500);
+        setLayout(new BorderLayout());
+
+        // Look & Feel
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+            SwingUtilities.updateComponentTreeUI(this);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
         setJMenuBar(eb);
-        this.add(ep);
+
+        final JTextArea myText = new JTextArea();
+        myText.setLineWrap(true);
+        JScrollPane myScroll = new JScrollPane(myText);
+
+        myContainer = this.getContentPane();
+        myContainer.setLayout(new BorderLayout());
+        myContainer.add(myScroll, BorderLayout.CENTER);
+
         this.setVisible(true);
     }
 }
--- src/view/EditorBar.java
+++ src/view/EditorBar.java
@@ -1,7 +1,11 @@
 package view;
 
+import function.File.Open;
+import function.File.Save;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 public class EditorBar extends JMenuBar {
@@ -17,15 +21,27 @@ public class EditorBar extends JMenuBar {
 
         // Create function.File Menu Field
         openMenuItem = new JMenuItem("Open");
+        openMenuItem.addActionListener(new PushAction());
         openMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
-        // TODO: event add
         fileMenu.add(openMenuItem);
 
         saveMenuItem = new JMenuItem("Save");
+        saveMenuItem.addActionListener(new PushAction());
         saveMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
-        // TODO: event add
         fileMenu.add(saveMenuItem);
 
         setVisible(true);
+    }
+
+    public class PushAction implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == saveMenuItem) {
+                // Save.save(editorTextArea.getText());
+            }
+            else if(e.getSource() == openMenuItem) {
+                // editorTextArea.setText(Open.open());
+            }
+        }
     }
 }
